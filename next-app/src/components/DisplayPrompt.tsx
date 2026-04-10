f372fd165f8186e6e7e130f4c9a81dd70d5335fc
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-10 06:25:41 +0000

    update: いらないフラグメント削除 add: DisplayUserInput配置

--- next-app/src/components/DisplayPrompt.tsx
+++ next-app/src/components/DisplayPrompt.tsx
@@ -1,20 +1,17 @@
 'use client';
 import { useState, useEffect } from 'react';
-import useCatchKey from '@/hooks/useCatchKey';
+import DisplayUserInput from './DisplayUserInput';
 
 export default function DisplayPrompt() {
   const [currentDirectory, setCurrentDirectory] = useState<string>('~');
-  const lastKeys = useCatchKey();
 
   return (
-    <>
-      <div>
-        <span className="text-[#21c080] font-bold">guest@Ohmura&apos;s-portfolio-site</span>
-        <span>:</span>
-        <span className="text-[#3b8eea]">{currentDirectory}</span>
-        <span>$</span>
-        <span>{lastKeys}</span>
-      </div>
-    </>
+    <div>
+      <span className="text-[#21c080] font-bold">guest@Ohmura&apos;s-portfolio-site</span>
+      <span>:</span>
+      <span className="text-[#3b8eea]">{currentDirectory}</span>
+      <span>$</span>
+      <DisplayUserInput />
+    </div>
   );
 }
