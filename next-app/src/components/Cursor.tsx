dc25c010bb80d6d92e7f132989c65f12db83f9d4
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-10 06:19:18 +0000

    add: Cursorコンポーネント

---
+++ next-app/src/components/Cursor.tsx
@@ -0,0 +1,7 @@
+export default function Cursor() {
+  return (
+    <span className="w-[9.6] h-[21.5] inline-block">
+      <span className="bg-white! inline-block w-full h-full align-middle"></span>
+    </span>
+  );
+}
