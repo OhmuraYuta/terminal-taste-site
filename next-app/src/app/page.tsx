3d8a9ef85f91ffd508123ddc6f66ffb71ec7e9ec
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-11 06:19:20 +0000

    add: History配置

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,11 +1,13 @@
-import ActivPrompt from '@/components/ActivPrompt';
 import { Roboto_Mono } from 'next/font/google';
+import ActivPrompt from '@/components/ActivPrompt';
+import History from '@/components/History';
 
 const robotoMono = Roboto_Mono();
 
 export default function Home() {
   return (
     <div className={`bg-[#1e1e1e] h-screen text-white ${robotoMono.className}`}>
+      <History />
       <ActivPrompt />
     </div>
   );
