ccdcf6240bb7cc9d3573ced3aee74ca77e6aeaee
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-11 06:36:33 +0000

    add: 下まで行ったとき自動でスクロールできる

--- next-app/src/components/ActivPrompt.tsx
+++ next-app/src/components/ActivPrompt.tsx
@@ -1,10 +1,17 @@
 'use client';
+import { useEffect, useRef } from 'react';
 import Prompt from './Prompt';
 import UserInput from './UserInput';
+import useUserInput from '@/hooks/useUserInput';
 
 export default function ActivPrompt() {
+  const scrollBottomRef = useRef<HTMLDivElement>(null);
+  const { history } = useUserInput();
+  useEffect(() => {
+    scrollBottomRef.current?.scrollIntoView({ behavior: 'instant' });
+  }, [history]);
   return (
-    <div>
+    <div ref={scrollBottomRef}>
       <Prompt currentDirectory="~" />
       <UserInput />
     </div>
