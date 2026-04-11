8d83d2fa4030b3337bcee877c7986c37b13733ad
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-11 04:21:51 +0000

    update: ActivPromptを配置

--- next-app/src/app/page.tsx
+++ next-app/src/app/page.tsx
@@ -1,4 +1,4 @@
-import DisplayPrompt from '@/components/DisplayPrompt';
+import ActivPrompt from '@/components/ActivPrompt';
 import { Roboto_Mono } from 'next/font/google';
 
 const robotoMono = Roboto_Mono();
@@ -6,7 +6,7 @@ const robotoMono = Roboto_Mono();
 export default function Home() {
   return (
     <div className={`bg-[#1e1e1e] h-screen text-white ${robotoMono.className}`}>
-      <DisplayPrompt />
+      <ActivPrompt />
     </div>
   );
 }
