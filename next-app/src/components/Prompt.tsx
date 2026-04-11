4dd12b927e89f93f53a1828b325dec5635521faa
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-11 04:16:11 +0000

    update: 名前を改善しプロンプトのみ表示 カレントディレクトリはpropsから受け取る

---
+++ next-app/src/components/Prompt.tsx
@@ -0,0 +1,10 @@
+export default function Prompt(props: { currentDirectory: string }) {
+  return (
+    <span>
+      <span className="text-[#21c080] font-bold">guest@Ohmura&apos;s-portfolio-site</span>
+      <span>:</span>
+      <span className="text-[#3b8eea]">{props.currentDirectory}</span>
+      <span>$</span>
+    </span>
+  );
+}
