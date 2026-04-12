45f278b3348eba092aa187273f31a0e26afae5fd
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-12 06:04:52 +0000

    add: allowedDevOrigins

--- next-app/next.config.ts
+++ next-app/next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  allowedDevOrigins: ['192.168.11.140'],
 };
 
 export default nextConfig;
