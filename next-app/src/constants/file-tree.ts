054a8a1b34017a6fdd479df71561f7b72a8964ab
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-13 12:15:54 +0000

    update: ファイルツリーも型定義に合わせて変更

--- next-app/src/constants/file-tree.ts
+++ next-app/src/constants/file-tree.ts
@@ -1,16 +1,43 @@
-import type { File } from '@/types/file-tree';
+import type { FileTree } from '@/types/file-tree';
 
-const FileTree: File[] = [
+export const FILE_TREE: FileTree = [
+  // 先頭は必ずrootにする
   {
-    fileType: 'directory',
-    name: 'kwus',
-    description: 'SEOの対策ツール',
-    parent: '~',
+    name: '~',
+    inode: 5,
+    content: [
+      {
+        name: 'sumi',
+        inode: 0,
+      },
+      {
+        name: 'nose',
+        inode: 3,
+      },
+    ],
   },
   {
-    fileType: 'directory',
-    name: 'youtube-downloader',
-    description: 'YouTubeダウンローダー',
-    parent: '~',
+    name: 'sumi',
+    inode: 0,
+    content: [
+      {
+        name: 'nazu',
+        inode: 1,
+      },
+      {
+        name: 'toto',
+        inode: 2,
+      },
+    ],
   },
-] as const;
+  {
+    name: 'nose',
+    inode: 3,
+    content: [
+      {
+        name: 'noah',
+        inode: 4,
+      },
+    ],
+  },
+];
