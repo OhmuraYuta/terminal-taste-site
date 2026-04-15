adcf35fdfc05dccc570db98b01e633e0bbd76013
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-15 18:30:37 +0000

    add: lsコマンド

--- next-app/src/lib/handleCommand.ts
+++ next-app/src/lib/handleCommand.ts
@@ -1,5 +1,14 @@
+import ls from './ls';
+
 export default function handleCommand(inputString: string, currentDirectory: string) {
   const command = inputString.split(' ')[0];
-  const result = `${command}: command not found`;
+  const arg = inputString.split(' ')[1];
+  console.log({ inputString, command });
+  let result = '';
+  if (command === 'ls') {
+    result = ls(inputString.split(' ')[1], currentDirectory);
+  } else {
+    result = `${command}: command not found`;
+  }
   return { result, currentDirectory };
 }
---
+++ next-app/src/lib/ls.ts
@@ -0,0 +1,82 @@
+import { FILE_TREE } from '@/constants/file-tree';
+import type { DirectoryContent } from '@/types/file-tree';
+import React from 'react';
+
+type ChildDirs = DirectoryContent[] | React.ReactElement;
+
+export default function ls(arg: string, currentDirectory: string): string {
+  let path = '';
+  if (arg) {
+    path = currentDirectory + '/' + arg;
+  } else {
+    path = currentDirectory;
+  }
+  const info = getInfoFromPath(path);
+  console.log(info);
+  if (Array.isArray(info?.childDirs)) {
+    const result = info.childDirs.map((dir) => {
+      return dir.name;
+    });
+    return result.join(' ');
+  }
+  return `ls: cannot access '${arg}': No such file or directory`;
+}
+
+function getInfoFromPath(path: string) {
+  console.log(path);
+  if (path === '~') {
+    path += '/';
+  }
+  const pathArray = path.split('/');
+  console.log(pathArray);
+  if (pathArray[pathArray.length - 1] === '') {
+    pathArray.splice(pathArray.length - 1, 1);
+  }
+  // . と .. を処理
+  for (let i = pathArray.length - 1; i >= 0; i--) {
+    if (pathArray[i] === '.') {
+      pathArray.splice(i, 1);
+    }
+  }
+  let dotDotIndex = pathArray.indexOf('..');
+  while (dotDotIndex !== -1) {
+    if (dotDotIndex === 1) {
+      // rootの親はない
+      pathArray.splice(dotDotIndex, 1);
+    } else {
+      pathArray.splice(dotDotIndex - 1, 2);
+    }
+    dotDotIndex = pathArray.indexOf('..');
+  }
+  console.log(pathArray);
+
+  // rootのinode
+  let inode = 5;
+  let childDirs: ChildDirs = [];
+  for (let i = 0; i < pathArray.length; i++) {
+    // 初回はrootのcontent取得
+    if (i === 0) {
+      childDirs = FILE_TREE[0].content;
+      continue;
+    }
+    if (!Array.isArray(childDirs)) {
+      return;
+    }
+    for (let child_i = 0; child_i < childDirs.length; child_i++) {
+      if (childDirs[child_i].name === pathArray[i]) {
+        inode = childDirs[child_i].inode;
+        for (const dir of FILE_TREE) {
+          if (dir.inode === inode && Array.isArray(dir.content)) {
+            childDirs = dir.content;
+            break;
+          }
+        }
+        break;
+      } else if (child_i === childDirs.length - 1) {
+        // 途中でbreakせず最後まで来たとき
+        return { error: 'error' };
+      }
+    }
+  }
+  return { inode, childDirs };
+}
