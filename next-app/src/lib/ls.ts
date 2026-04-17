f2be1854434d5c521036c139cc38a833cdb2e161
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-17 06:21:40 +0000

    fix: argがundefinedの時を考慮

--- next-app/src/lib/ls.ts
+++ next-app/src/lib/ls.ts
@@ -2,7 +2,7 @@ import parsePath from './parsePath';
 
 export default function ls(arg: string, currentDirectory: string): string {
   let path = '';
-  if (arg.startsWith('~')) {
+  if (arg && arg.startsWith('~')) {
     path = arg;
   } else if (arg) {
     path = currentDirectory + '/' + arg;
