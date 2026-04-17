506d6551125ad30e7f93778aef62bd589a543d68
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-17 03:36:24 +0000

    update: ~から始まる時を考慮

--- next-app/src/lib/ls.ts
+++ next-app/src/lib/ls.ts
@@ -2,7 +2,9 @@ import parsePath from './parsePath';
 
 export default function ls(arg: string, currentDirectory: string): string {
   let path = '';
-  if (arg) {
+  if (arg.startsWith('~')) {
+    path = arg;
+  } else if (arg) {
     path = currentDirectory + '/' + arg;
   } else {
     path = currentDirectory;
