25870845ed0bcbf289fb791a5a9939b46fd188b8
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-24 03:32:21 +0000

    update: History typeに対応

--- next-app/src/lib/handleCommand.ts
+++ next-app/src/lib/handleCommand.ts
@@ -1,29 +1,43 @@
 import ls from './ls';
 import cd from './cd';
 import cat from './cat';
+import type { LsResult } from '@/types/history';
 
-export default function handleCommand(inputString: string, currentDirectory: string) {
+type Res =
+  | {
+      type: 'normal' | 'content';
+      currentDirectory: string;
+      result: string;
+    }
+  | {
+      type: 'ls';
+      currentDirectory: string;
+      result: LsResult;
+    };
+
+export default function handleCommand(inputString: string, currentDirectory: string): Res {
   const command = inputString.split(' ')[0];
   console.log({ inputString, command });
   let result = '';
-  const type = 'normal';
   if (command === 'ls') {
     const res = ls(inputString.split(' ')[1], currentDirectory);
+    if (res.type === 'normal') {
+      return { type: res.type, result: res.result, currentDirectory };
+    }
     return { type: res.type, result: res.result, currentDirectory };
   } else if (command === 'cd') {
     const res = cd(inputString.split(' ')[1], currentDirectory);
-    if (res.type === 'success') {
-      currentDirectory = res.currentDirectory;
-    } else {
-      result = res.errorMsg;
-    }
+    return { type: res.type, currentDirectory: res.currentDirectory, result: res.result };
   } else if (command === 'cat') {
     const res = cat(inputString.split(' ')[1], currentDirectory);
+    if (res.type === 'normal') {
+      return { result: res.result, currentDirectory, type: res.type };
+    }
     return { result: res.result, currentDirectory, type: res.type };
   } else if (!inputString.trim()) {
     result = '';
   } else {
     result = `${command}: command not found`;
   }
-  return { result, currentDirectory, type };
+  return { result, currentDirectory, type: 'normal' };
 }
