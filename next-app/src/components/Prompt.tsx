c0ae7f693468ffb06750a16eda9f3dd95346984c
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-25 00:06:56 +0000

    add: プロンプトと入力の間に余白

--- next-app/src/components/Prompt.tsx
+++ next-app/src/components/Prompt.tsx
@@ -1,6 +1,6 @@
 export default function Prompt(props: { currentDirectory: string }) {
   return (
-    <span>
+    <span className="mr-2">
       <span className="text-[#21c080] font-bold">guest@Ohmura&apos;s-portfolio-site</span>
       <span>:</span>
       <span className="text-[#3b8eea]">{props.currentDirectory}</span>
