72ccb1afa9e2b0c6892f4c13338a79184dbeb625
OhmuraYuta <ohmura.y.24kdgn@gmail.com>
2026-04-25 00:08:44 +0000

    update: カレントディレクトリの表示を太く

--- next-app/src/components/Prompt.tsx
+++ next-app/src/components/Prompt.tsx
@@ -3,7 +3,7 @@ export default function Prompt(props: { currentDirectory: string }) {
     <span className="mr-2">
       <span className="text-[#21c080] font-bold">guest@Ohmura&apos;s-portfolio-site</span>
       <span>:</span>
-      <span className="text-[#3b8eea]">{props.currentDirectory}</span>
+      <span className="text-[#3b8eea] font-bold">{props.currentDirectory}</span>
       <span>$</span>
     </span>
   );
